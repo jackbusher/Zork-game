e1b8bc1c23e4b5f0e5d7982aa18ef8f45142295c
DeanKelly751 <62704110+DeanKelly751@users.noreply.github.com>
2021-04-18 13:38:58 +0100

    Update Character.h

--- zork/Character.h
+++ zork/Character.h
@@ -10,19 +10,30 @@ using std::vector;
 
 class Character {
 private:
-    string description;
-     vector < string > itemsInCharacter;
+     string description;
+     vector < string > itemsInCharacter;     
+     int health = 100;
+     int level = 0;
+
 public:
     void addItems(string Item);
 
+    void setHealth(int);
+    int getHealth();
 
+    void setLevel(int);
+    int getLevel();
 
+    void addItem(Item item);
+    void addItem(string description);
+    void addItem(Item &item);
+
+    void dropItem(Item item);
 
 public:
     Character(string description);
     string shortDescription();
     string longDescription();
-
-};
+    };
 
 #endif CHARACTER_H_
