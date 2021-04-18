499e0c2fce64707fdb5d2aac06753a15e44ab6a0
DeanKelly751 <62704110+DeanKelly751@users.noreply.github.com>
2021-04-18 16:50:25 +0100

    Update enemy.cpp

--- zork/enemy.cpp
+++ zork/enemy.cpp
@@ -1,41 +1,31 @@
-#include "pch.h"
 #include "enemy.h"
-#include <fstream>
+#include <string>
 #include <iostream>
+using namespace std;
 
-
-
-
-Enemy::Enemy(string name, string description, Room* room) :
-
-{
-    SetStats(name);
-    type = ENEMY;
+enemy::enemy(string enDescription){
+    this->description = enDescription;
+    enemyhealth = 100;
+   enemylevel = 0;
+    enemydamage = 25;
 }
 
-
-Enemy::~Enemy()
-{
+void enemy::addDamage(int damage) {
+    enemyhealth -= damage;
 }
 
-void Enemy::SetStats(string name) {
-    //read stats from the Bestiary files and set on this class parameter
-    std::ifstream i("Bestiary.json");
+string enemy::getDescription(){
+    return description;
+}
 
-    i >> j;
-    try {
-        auto bestiary = j[name];
-        lvl = bestiary["lvl"];
-        str = bestiary["str"];
-        dex = bestiary["dex"];
-        con = bestiary["con"];
-        exp = bestiary["exp"];
-        ac = bestiary["ac"];
-        max_health = con * lvl;
-        SetMods();
-        health = max_health;
-    }
-    catch (exception) {
-        //enemy not found
-    }
+int enemy::getHealth() {
+    return enemyhealth;
+}
+
+/*void enemy::setDescription(string description) {
+    this->description = description;
+} */
+
+void enemy::setHealth(int enHealth) {
+    enemyhealth = enHealth;
 }
